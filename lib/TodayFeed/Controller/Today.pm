93a4d2438026566417b6ea4075545e67bb331365
Dmitry Shelestov <indy.ru@gmail.com>
2018-01-16 22:58:04 +0300

    Changes

--- lib/TodayFeed/Controller/Today.pm
+++ lib/TodayFeed/Controller/Today.pm
@@ -12,9 +12,6 @@ sub feed {
     ( $feed_params->{$param} = uc $self->param($param) ) =~ s/\s+//g;
   }
 
-  $feed_params->{start_date} = $feed_params->{end_date} =
-    delete $feed_params->{date};
-
   my $parser = iTunes::Today->new( $self->app->config('itunes_today') );
   my $result = $parser->parse(%$feed_params);
 
