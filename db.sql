ee3f91e88f32bd91ec9f56b72e42a19da0303ddc
indjke <35488091+indjke@users.noreply.github.com>
2018-01-17 10:15:38 +0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -2,7 +2,10 @@ CREATE TABLE IF NOT EXISTS `stories` (
   `id` bigint NOT NULL,
   `label` text NOT NULL,
   `date` date NOT NULL,
-  `country` varchar(3) NOT NULL,
+  `country` varchar(2) NOT NULL,
   `json_source` TEXT,
   PRIMARY KEY (`id`)
 ) DEFAULT CHARSET=utf8;
+
+CREATE INDEX date_idx ON stories (date);
+CREATE INDEX country_idx ON stories (country);
